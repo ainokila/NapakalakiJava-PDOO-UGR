7027ca2afb17eea33fa3a5958b1a6a8ff310ac55
saytes <saytes@correo.ugr.es>
2015-11-05 18:49:36 +0100

    Añadidos consultores Treasure

--- Napakalaki/src/napakalaki/Treasure.java
+++ Napakalaki/src/napakalaki/Treasure.java
@@ -11,4 +11,25 @@ package napakalaki;
  */
 public class Treasure {
     
+    private String name ;
+    private int bonus ;
+    private TreasureKind type;
+    
+    public Treasure(String n, int bonus, TreasureKind t){
+           name = n;
+           this.bonus = bonus;
+           type = t;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getBonus(){
+        return bonus;
+    }
+    
+    public TreasureKind getType(){
+        return type;
+    }
 }
